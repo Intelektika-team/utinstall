5bb3770aecfe237e791f4451a11d83ab5756d0c0
Intelektika <kvantorium73.int@gmail.com>
2025-08-05 17:27:46 +0400

    Update script_sample.py

--- script_sample.py
+++ script_sample.py
@@ -7,7 +7,10 @@ def main(*args):
     Main script function (required entry point).
     Processes command line arguments and executes appropriate functions.
     """
-    if args[0] == "test":
-        return print_hello()
-    else:
-        print("No valid command provided.")
+    try:
+        if args[0] == "test":
+            return print_hello()
+        else:
+            print("No valid command provided.")
+    except Exception as e:
+        print(f"Error in parse arguments. {e}")
